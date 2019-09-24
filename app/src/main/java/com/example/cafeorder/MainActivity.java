8de12ef23c24439d57c40ac6591b5d4427700f0b
Trojansky <33102461+alextrojansky@users.noreply.github.com>
2019-09-24 22:12:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cafeorder/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.cafeorder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText editTextName;
+    public EditText editTextPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+    }
+
+    public void onClickCreateOrder(View view) {
+        String name = editTextName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (!name.isEmpty() && !password.isEmpty()) {
+            Intent intent = new Intent(this, CreateOrderActivity.class);
+
+            intent.putExtra("name", name);
+            intent.putExtra("password", password);
+            startActivity(intent);
+            
+        } else {
+            Toast.makeText(this, R.string.warning_fill_fields, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
